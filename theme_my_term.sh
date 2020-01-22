ae353c84c9b44d58b1493ba96a75fffa9e4313c6
Adam M. Jacobs <ajacobs@pa.msu.edu>
2020-01-22 08:57:33 -0500

    Add basic util for themeing terminal emulator colors

---
+++ theme_my_term.sh
@@ -0,0 +1,17 @@
+# This script utilizes the amazing project here:
+#   github.com/Mayccoll/Gogh
+# See that for more details.  
+#
+# This works for XFCE4 terminal emulator in Arch Linux for me.
+# Supposed to work for other major linux terminal emulators and iTerm2
+#
+# Requires: 
+#   bash (doesn't have to be your shell, just available), 
+#   dconf-cli (or similar), and 
+#   uuid-runtime
+#
+# What this does:
+#   A sampling of color themes/profiles will be presented.  You can try them and
+#   apply them.  It should "just work" if you satisfy the conditions above.
+
+bash -c "$(wget -qO- https://git.io/vQgMr)"

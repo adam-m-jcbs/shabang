5e496abdf924167e1cd1a0478d717c68d12b8ae0
Adam M. Jacobs <adam.m.jcbs@gmail.com>
2020-06-30 14:18:52 -0400

    Add some docs to script

--- xev_kb_rec.sh
+++ xev_kb_rec.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
 
+#Requirements:
+#   You need `xev`, which on Arch is
+#   $ pacman -Syu xorg-xev
+#
+#   Most *nixes should come with awk
+
 #Use xev to monitor/echo key presses and the keycodes associated with them
+#This incantation is originally from the Arch Wiki.
 xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'

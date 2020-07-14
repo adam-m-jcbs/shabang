749218272947337e03126052be3ed7c08c51bb98
Adam M. Jacobs <adam.m.jcbs@gmail.com>
2020-07-14 16:52:06 -0400

    ugh, more fixes being tossed about through git, this time I made sure there is both a full log and context-specific logs

--- clin.zsh
+++ clin.zsh
@@ -36,12 +36,15 @@
 #
 #Use matching regex (date) sentinel to get state
 cur_state=`cat ~/.clock_log/clin_time        | awk '{$1=$1;print}'` #awk magic ($1=$1 has side-effect of re-evaluating $0 and rebuilding with default separators) trims outer spaces and squeezes internal spaces to 1
+#Use matching regex (three capital letter code) sentinel to get context (optional! if blank, ignore)
+cur_context=`cat ~/.clock_log/clin_time      | awk '$1 ~ /[A-Z]{3}:/             {$1=$1;print}'` 
+cur_context_code=`cat ~/.clock_log/clin_time | awk '$1 ~ /[A-Z]{3}:/             {$1=$1;printf "%.3s", $1}'` 
+cur_context_desc=`cat ~/.clock_log/clin_time | awk '$1 ~ /[A-Z]{3}:/             {$1="";print $0}'` 
 if [[ $cur_state == 'OUT' ]]; then
     #We're here, so state was clocked out.
     #Good, that's what's expected for a clock in operation.
 
     #If the optional context is provided, prepare directories
-    cur_context='NULL'
     if [[ $# -gt 0 ]]; then
         #TODO: Assuming proper form for now, need to error-check
         #Take all arguments except the first/0th (name of script)
@@ -61,14 +64,24 @@ if [[ $cur_state == 'OUT' ]]; then
     fi
     
     #Store any clocked out log in ~/.clock_log/YYYY-MM-DD.log
+    #Also, if a context code is given, store context-specific notes in their logs
     if [[ -f ~/.clock_log/clout_notes ]]; then
-        cat ~/.clock_log/clout_notes >>  ~/.clock_log/${with_slash}`date +"%F"`.log
+        if [[ -n $cur_context_code ]]; then
+            echo "Context code: $cur_context_code" >>  ~/.clock_log/`date +"%F"`.log
+            cat ~/.clock_log/clout_notes >>  ~/.clock_log/`date +"%F"`.log
+            cat ~/.clock_log/clout_notes >>  ~/.clock_log/${with_slash}`date +"%F"`.log
+        else
+            cat ~/.clock_log/clout_notes >>  ~/.clock_log/`date +"%F"`.log
+        fi
         rm ~/.clock_log/clout_notes
     fi
 
     date +"%F %H:%M:%S" > ~/.clock_log/clin_time
-    echo $cur_context >> ~/.clock_log/clin_time
-    with_parens=\(${cur_context_code}\)
+    with_parens=''
+    if [[ -n $cur_context_code ]]; then
+        echo $cur_context >> ~/.clock_log/clin_time
+        with_parens=\(${cur_context_code}\)
+    fi
     date +"IN${with_parens} %H:%M" > ~/.clock_log/state
 
     #The state's updated,
@@ -76,6 +89,8 @@ if [[ $cur_state == 'OUT' ]]; then
     #file, so this is all we need to do to update the prompt
 else
     echo "You've already clocked in!"
+    #We can now be guaranteed the state info is available, so grab it
+    cur_state=`cat ~/.clock_log/clin_time        | awk '$1 ~ /[0-9]{4}-[0-9]{2}-[0-9]{2}/ {$1=$1;print}'` #awk magic ($1=$1 has side-effect of re-evaluating $0 and rebuilding with default separators) trims outer spaces and squeezes internal spaces to 1
     echo "    IN@  " $cur_state
     if [[ -n $cur_context ]]; then
         echo "    ctx: " $cur_context
